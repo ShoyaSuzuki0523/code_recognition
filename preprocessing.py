1a4b63c0d9a9058d288ff5d6f135d0d895e8548f
shoya0523 <bellwood0523@gmail.com>
2020-07-20 22:22:13 +0900

    前処理クラスにクローズ機能の追加

--- preprocessing.py
+++ preprocessing.py
@@ -56,7 +56,7 @@ class Preprocessing():
 			"bm"
 		]
 	
-	def compile_sound(self, plot=True):
+	def compile_sound(self, train_range=[1,2,3,4,5,6,7,8], test_range=[9,10], close=True):
 		for i, cf in enumerate(self.codeFiles):
 			folder_path = self.sound_path + cf + "/"
 			folder = os.listdir(folder_path)
@@ -86,19 +86,19 @@ class Preprocessing():
 				print(y_data)
 				print("=============================================")
 
-				if plot == True:
-					fl = len(Chromas[0])
+				# if plot == True:
+				# 	fl = len(Chromas[0])
 
-					fig = plt.figure()
-					x = np.arange(0, fl, 1)
-					for i, note in enumerate(self.notes) :
-						ax = fig.add_subplot(4,3,i + 1)
-						ax.plot(x, Chromas[i])
-						ax.set_title(note)
-						ax.set_ylim(0,1.1)
+				# 	fig = plt.figure()
+				# 	x = np.arange(0, fl, 1)
+				# 	for i, note in enumerate(self.notes) :
+				# 		ax = fig.add_subplot(4,3,i + 1)
+				# 		ax.plot(x, Chromas[i])
+				# 		ax.set_title(note)
+				# 		ax.set_ylim(0,1.1)
 
-					fig.subplots_adjust(hspace=0.4)
-					plt.show()
+				# 	fig.subplots_adjust(hspace=0.4)
+				# 	plt.show()
 
 				np.savetxt(fname=x_output, X=trans_Chromas, fmt="%.18e")
 				np.savetxt(fname=y_output, X=y_data, fmt="%s")
@@ -109,12 +109,15 @@ class Preprocessing():
 		y_train = np.empty(1)
 		y_test = np.empty(1)
 
+		if close == True:
+			test_range = train_range + test_range
+
 		for c in self.codeFiles:
-			for i in range(1,8):
+			for i in train_range:
 				x_train = np.vstack([x_train, np.loadtxt(self.x_output_path + c + "_" + str(i) + ".txt")])
 				y_train = np.hstack([y_train, np.loadtxt(self.y_output_path + c + "_" + str(i) + ".txt", dtype=str)])
 				print("loadhing " + self.x_output_path + c + "_" + str(i) + ".txt" + " and " + self.y_output_path + c + "_" + str(i) + ".txt")
-			for i in range(9,10):
+			for i in test_range:
 				x_test = np.vstack([x_test, np.loadtxt(self.x_output_path + c + "_" + str(i) + ".txt")])
 				y_test = np.hstack([y_test, np.loadtxt(self.y_output_path + c + "_" + str(i) + ".txt", dtype=str)])
 				print("loadhing " + self.x_output_path + c + "_" + str(i) + ".txt" + " and " + self.y_output_path + c + "_" + str(i) + ".txt")
--- test/test.py
+++ test/test.py
@@ -1,7 +1,5 @@
-import os
 import numpy as np
-import pandas as pd
 
-x_train = np.zeros(12)
+a = np.array([1,2,3,4,5])
 
-print(x_train)
+print(a)
